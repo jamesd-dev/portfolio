62cc9b883aa7a82aee55d6c7e76940a110a31b92
james <dawsonjames027@gmail.com>
2021-05-03 17:25:02 +0100

    positioned avatar imaged and started on neon size

--- src/components/sections/Title.js
+++ src/components/sections/Title.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import Confetti from '../../classes/Confetti';
 import './Title.scss';
+import profileImage1 from "../../images/profile1.png"; 
 
 function Title() {
 const [dummyState, setDummyState] = useState(0);
@@ -8,7 +9,7 @@ const [dummyState, setDummyState] = useState(0);
 window.onresize = () => setDummyState(Math.random());
 
 useEffect(() => {
-    let container = document.getElementById("title-container");
+    let container = document.getElementById("confetti-container");
     while (container.firstChild) {
         container.removeChild(container.lastChild);
       }
@@ -19,6 +20,9 @@ useEffect(() => {
 
     return (
         <div id="title-container">
+        <div id="confetti-container"/>
+        <img src={profileImage1} alt="me" id="profile-image"/>
+        <div className="logo"><b>J<span>am</span>es<span> D</span>aw<span>son</span></b></div>
         </div>
     );
 }
