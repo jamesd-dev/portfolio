d813ed000ab963da9146a753dcb1b20328dee9ce
james <dawsonjames027@gmail.com>
2021-05-03 14:27:01 +0100

    completed confetti animation

--- src/classes/Confetti.js
+++ src/classes/Confetti.js
@@ -1,17 +1,28 @@
 import palette from '../theme.json';
 
 const colorPalette = [palette.primary1, palette.primary3, palette.accent1];
+const maxSpeed = 4; // seconds to reach bottom
+const minSpeed = 8;
+const maxSize = 5; // long edge of the confetti
+const minSize = 3;
 
 class Confetti {
     constructor(container) {
-        this.radius = (Math.random() * 8) + 2;
-        this.vy = (Math.random()) + 1;
+
+        this.length = (Math.random() * maxSize) + minSize;
+        this.vy = minSpeed - (Math.random() * maxSpeed);
         this.color = colorPalette[Math.floor(Math.random() * colorPalette.length)];
         this.x = Math.random() * container.offsetWidth;
         this.y = -this.radius;
         this.element = document.createElement('div');
         this.element.classList.add("confetti");
         container.appendChild(this.element);
+        this.element.style.left = this.x + "px";
+        this.element.style.width = this.length + "px";
+        this.element.style.height = this.length * 1.618 + "px";
+        this.element.style.animationDuration = this.vy + "s";
+        this.element.style.animationDelay =  -(Math.random() * this.vy) + "s";
+        this.element.style.backgroundColor = this.color;
     }
 }
 
--- src/components/sections/Title.js
+++ src/components/sections/Title.js
@@ -1,12 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Confetti from '../../classes/Confetti';
 import './Title.scss';
 
 function Title() {
-    
+const [dummyState, setDummyState] = useState(0);
+
+window.onresize = () => setDummyState(Math.random());
+
 useEffect(() => {
     let container = document.getElementById("title-container");
-new Confetti(container);
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+      }
+    for(let i = 0; i < (container.offsetWidth / 10); i++) {
+        new Confetti(container);
+    }
 });
 
     return (
